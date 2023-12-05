724d555e6da1384842adae07cf5322b740b00871
6alaam <ballaam@yandex.ru>
2023-12-05 10:29:58 +0300

    Add files via upload

---
+++ Seminar_7/Task_1/Program.cs
@@ -0,0 +1,245 @@
+﻿// здесь разбор задач из 4 и 5 семинаров но уже собранные в одну прогу и визуально понятное отображение
+
+Main();
+
+void Main()
+{
+    bool isWork = true;
+    while (isWork)
+    {
+        Console.WriteLine("enter command");
+        string command = Console.ReadLine();
+        switch (command)
+        {
+            case "Task 25":
+                Task25();
+                break;
+            case "Task 27":
+                Task27();
+                break;
+            case "Task 29":
+                Task29();
+                break;
+            case "Task 34":
+                Task34();
+                break;
+            case "Task 36":
+                Task36();
+                break;
+            case "Task 38":
+                Task38();
+                break;
+            case "exit":
+                isWork = false;
+                break;
+
+        }
+    }
+}
+
+// задача 25 , напишите цикл,который принемает на вход два числа (A и В)
+//  и возводит а в степень б
+
+void Task25()
+{
+    System.Console.WriteLine("Task 25");
+
+    int number = ReadInt("number");
+    int pow = ReadInt("Pow");
+
+    Console.WriteLine("Pow:");
+
+    Console.WriteLine(Pow(number, pow));
+
+    Console.WriteLine("RecPow:");
+    Console.WriteLine(PowRecursion(number, pow));
+
+
+}
+// принемает на вход число и выдает сумму всех числе в нем
+void Task27()
+{
+    Console.WriteLine("Task 27");
+    int number = ReadInt("number");
+    Console.WriteLine(GetDigitSum(number));
+
+}
+// здайте одномерный массив из 8 символов и распечатйте его
+
+void Task29()
+{
+    Console.WriteLine("Task 29");
+    int[] array = CreateArray(8, 1, 10);
+    Console.WriteLine(string.Join(", ", array));
+}
+
+
+
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу,
+//  которая покажет количество чётных чисел в массиве.
+
+
+void Task34()
+{
+    Console.WriteLine("Task 34");
+    int length = ReadInt("Length");
+    int[] array = CreateArray(length, 100, 1000);
+    Console.WriteLine(string.Join(", ", array));
+    Console.WriteLine(GetCount(array));
+
+}
+
+
+
+
+// Задача 36: Задайте одномерный массив, 
+// заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
+
+
+void Task36()
+{
+    Console.WriteLine("Task 36");
+    int length = ReadInt("Length");
+    int[] array = CreateArray(length, 100, 1000);
+    Console.WriteLine(string.Join(", ", array));
+    Console.WriteLine(GetSum(array));
+
+}
+
+// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементами массива
+
+void Task38()
+{
+    Console.WriteLine("Task 36");
+    int length = ReadInt("Length");
+    double[] array = CreateDoubleArray(length, 100, 1000);
+    Console.WriteLine(string.Join(", ", array));
+    Console.WriteLine(GetDifferenceBetweenMinAndMax(array));
+
+}
+
+
+int ReadInt(string argument)
+{
+
+    Console.Write($"Input {argument}: ");
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("Не число введите число!");
+
+    }
+    return number;
+
+}
+
+int Pow(int number, int pow)
+{
+    int result = 1;
+    for (int i = 1; i <= pow; i++)
+    {
+        result *= number;
+
+    }
+    return result;
+}
+int PowRecursion(int number, int pow)
+{
+    if (pow <= 1) return number;
+    return number * PowRecursion(number, pow - 1);
+
+}
+
+int GetDigitSum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+int[] CreateArray(int length, int min, int max)
+{
+
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+double[] CreateDoubleArray(int length, int min, int max)
+{
+
+
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1) + Math.Round(new Random().NextDouble(), 2);
+    }
+    return array;
+
+
+
+}
+
+int GetCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+
+        }
+
+    }
+    return count;
+}
+
+int GetSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            sum += arr[i];
+
+        }
+
+    }
+    return sum;
+}
+
+double GetDifferenceBetweenMinAndMax(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        else if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return Math.Round(max - min, 2);
+}
+
+
+
+
+
+
---
+++ Seminar_7/Task_2/Program.cs
@@ -0,0 +1,227 @@
+﻿
+
+
+Main();
+
+void Main()
+{
+    bool isWork = true;
+    while (isWork)
+    {
+        Console.WriteLine("enter command");
+        string command = Console.ReadLine();
+        switch (command)
+        {
+            case "Task 46":
+                Task46();
+                break;
+            case "Task 48":
+                Task48();
+                break;
+            case "Task 49":
+                Task49();
+                break;
+            case "Task 51":
+                Task51();
+                break;
+            // case "Task 36":
+            //     Task36();
+            //     break;
+            // case "Task 38":
+            //     Task38();
+            //     break;
+            case "exit":
+                isWork = false;
+                break;
+
+        }
+    }
+}
+
+// задайте целый двумерный массив размером m*n заполненный случайными числами (целыми)
+
+void Task46()
+{
+
+    Console.WriteLine("Task 46");
+    System.Console.WriteLine("Введите число строк m");
+    int m = ReadInt("m");
+    System.Console.WriteLine("Введите число столбцов n");
+    int n = ReadInt("n");
+    int[,] array = CreateBenaryArray(m, n, 10, 99);
+    PrintArray(array);
+
+}
+
+// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: A = m+n.
+// Выведите полученный массив на экран.
+
+
+void Task48()
+{
+    Console.WriteLine("Task 48");
+    System.Console.WriteLine("Введите число строк m");
+    int m = ReadInt("m");
+    System.Console.WriteLine("Введите число столбцов n");
+    int n = ReadInt("n");
+    int[,] array = CreateSumMNelementsArray(m, n);
+    PrintArray(array);
+
+
+
+}
+
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+void Task49()
+{
+    Console.WriteLine("Task 49");
+    System.Console.WriteLine("Введите число строк m");
+    int m = ReadInt("m");
+    System.Console.WriteLine("Введите число столбцов n");
+    int n = ReadInt("n");
+    int[,] array = CreateBenaryArray(m, n);
+    PrintArray(array);
+    System.Console.WriteLine(" Переделанный массив");
+    EvenNumberedSquare(array);
+    PrintArray(array);
+
+}
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+
+void Task51()
+{
+    Console.WriteLine("Task 51");
+    System.Console.WriteLine("Введите число строк m");
+    int m = ReadInt("m");
+    System.Console.WriteLine("Введите число столбцов n");
+    int n = ReadInt("n");
+    int[,] array = CreateBenaryArray(m, n);
+    PrintArray(array);
+    Console.Write("сумма главной диангонали ");
+    
+    Console.WriteLine(ArrayDiagonalSum(array));
+
+}
+
+
+
+
+
+
+
+
+int ReadInt(string argument)
+{
+
+    Console.Write($"Input {argument}: ");
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("Не число введите число!");
+
+    }
+    return number;
+
+}
+
+void PrintArray(int[,] matr)        /// печать двумерного массива
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+
+int[,] CreateBenaryArray(int m, int n, int min = 2, int max = 6)
+{
+
+    int[,] array = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+
+
+
+        for (int j = 0; j < n; j++)
+        {
+
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return array;
+}
+
+int[,] CreateSumMNelementsArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+
+
+
+        for (int j = 0; j < n; j++)
+        {
+
+            array[i, j] = i + j;
+        }
+
+    }
+    return array;
+
+}
+
+
+int[,] EvenNumberedSquare(int[,] array)
+{
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = (array[i, j]) * (array[i, j]);
+            }
+
+        }
+
+    }
+    return array;
+}
+
+int ArrayDiagonalSum(int[,] array)
+{
+
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += array[i,j];
+            }
+
+        }
+
+    }
+    return sum;
+
+}
+
